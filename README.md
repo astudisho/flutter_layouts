338093802722ca2557c69d55f53400c492e14b8c
Javier Astudillo <javier_astudillo@live.com.mx>
2020-04-06 23:54:13 -0500

    Initial commit with only the interface

---
+++ README.md
@@ -0,0 +1,21 @@
+# flutter_layouts
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+
+This is a test project to show the use of layouts in flutter following Simone De alessandria course in Pluralsight
+
+[Creating layouts with flutter Pluralsight](https://app.pluralsight.com/course-player?clipId=de72f34b-7a06-4676-8558-239fbae30f6f)
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Layouts",
+      darkTheme: ThemeData(primaryColor: Colors.purple),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.teal,
+        accentColor: Colors.grey,
+        
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Building layouts with Flutter"),
+        ),
+        body: Center(
+          child: Text(
+            "Hello flutter layouts!",
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.lightbulb_outline),
+          onPressed: () => floatingButtonHandler(),
+        ),
+        persistentFooterButtons: <Widget>[
+          IconButton(icon: Icon(Icons.add_comment), onPressed: null,),
+          IconButton(icon: Icon(Icons.alarm_add), onPressed: null,),
+          IconButton(icon: Icon(Icons.add_location), onPressed: null),
+        ],
+      ),
+    );
+  }
+
+  void floatingButtonHandler(){
+    print("You rang?");
+  }
+}
